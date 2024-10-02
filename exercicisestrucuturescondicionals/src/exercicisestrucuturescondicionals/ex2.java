082795742f6cd60ce19172e07599b4ed7e4757e5
Anna Patricia Aguilera Pérez <aag7815@proven.local>
2024-10-02 11:03:02 +0200

    1

---
+++ exercicisestrucuturescondicionals/src/exercicisestrucuturescondicionals/ex2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercicisestrucuturescondicionals;
+
+/**
+ *
+ * @author aag7815
+ */
+public class ex2 {
+    public static void main(String[] args) {
+        int lado=5;
+        int area= lado*lado;
+        System.out.println("L'area del quadrat es:" +area);
+                
+    }
+  
+}
